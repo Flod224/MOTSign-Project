ff0378b02365024cd51c196be34fd577a5ec7bd2
Mounir3014 <223014@umons.ac.be>
2024-05-19 16:43:29 +0200

    update .env

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import {View, Text} from "react-native";
 import Routes from './routes/Routes'; 
-import {OpenAIApi} from 'openai'
 import { REACT_APP_OPENAI_KEY } from '@env';
 
 const App = ()=>{
-    const openai = new OpenAIApi({
-        apiKey: process.env.REACT_APP_OPENAI_KEY,
-    });
+   console.log(REACT_APP_OPENAI_KEY)
     return (<View style= {{flex: 1}} >
-        <Routes openai={openai} />
+        <Routes> </Routes>
     </View>);
 };
 
--- ecrans/Text_to_Signs/Text_to_Signs.js
+++ ecrans/Text_to_Signs/Text_to_Signs.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { View, TextInput, Button, Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import { Video } from 'expo-av';
-//import OpenAI from "openai";
+import OpenAI from "openai";
 
-//const openai = new OpenAI();
+const openai = new OpenAI();
 
 const videopahtdefaults = 'https://storage.googleapis.com/motsign/assets/avatar.mp4';
 
@@ -20,6 +20,13 @@ const Text_to_Signs = () => {
   //const openaiClient = new openai.OpenAI({ apiKey: openaiApiKey });
 
   const correctSyntax = async (inputText) => {
+    const completion = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      prompt: 'Write a tagline for an ice cream shop.'
+  });
+
+  
+
     try {
       const response = await openai.chat.create({
         messages: [{ role: "system", content: "Correct this following text if any syntaxique error or leave it exactly same:"+inputText }],
@@ -38,7 +45,7 @@ const Text_to_Signs = () => {
     setLoading(true);
     try {
       const correctedText = await correctSyntax(text);
-      const response = await fetch('https://b7e9-188-188-52-77.ngrok-free.app//translate-text', {
+      const response = await fetch('https://4c2e-83-134-111-160.ngrok-free.app//translate-text', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
